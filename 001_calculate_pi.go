ace860fbbfa9fb59f81d196824eb1ab4f88cb647
robbie-baxter <robbie.baxter@salesforce.com>
2019-12-19 12:45:21 -0500

    completed main code

--- 001_calculate_pi.go
+++ 001_calculate_pi.go
@@ -5,22 +5,70 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"math/big"
 )
 
+//logic/code used from https://play.golang.org/p/hF9jklt5lp
 func main() {
+
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("How many digits of Pi do you want? ")
 	text, _ := reader.ReadString('\n')
 
-	digits, _ := strconv.ParseInt(text, 10, 64)
-	getPi(digits)
+	text = strings.TrimSuffix(text, "\n")
 
+	digits, _ := strconv.ParseInt(text, 10, 64)
+
+	pi := getPi(digits)
+
+	fmt.Println(pi)
 }
 
-func getPi(digits int64) string {
-	digits += 1
+func getPi(numberofdigits int64) string {
 
-	fmt.Println(digits)
+	digits := big.NewInt(500+10)
+	unity := big.NewInt(0)
+	unity.Exp(big.NewInt(10), digits, nil)
+	pi := big.NewInt(0)
+	four := big.NewInt(4)
+	pi.Mul(four, pi.Sub(pi.Mul(four, arccot(5, unity)), arccot(239, unity)))
+	//pi := arccot(numberofdigits, unity)
 
-	return "stuff"
+	piString := pi.String()
+
+	runes := []rune(piString)
+
+	piReturn := string(runes[0:1]) + "." + string(runes[1:numberofdigits])
+
+	return piReturn
+}
+
+func arccot(x int64, unity *big.Int) *big.Int {
+	bigx := big.NewInt(x)
+	xsquared := big.NewInt(x * x)
+	sum := big.NewInt(0)
+	sum.Div(unity, bigx)
+	xpower := big.NewInt(0)
+	xpower.Set(sum)
+	n := int64(3)
+	zero := big.NewInt(0)
+	sign := false
+
+	term := big.NewInt(0)
+	for {
+		xpower.Div(xpower, xsquared)
+		term.Div(xpower, big.NewInt(n))
+		if term.Cmp(zero) == 0 {
+			break
+		}
+		if sign {
+			sum.Add(sum, term)
+		} else {
+			sum.Sub(sum, term)
+		}
+		sign = !sign
+		n += 2
+	}
+	return sum
 }
