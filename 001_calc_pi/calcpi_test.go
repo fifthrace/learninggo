1bc67352197e5495c5d2b3b03cdb60e9c0dadbd4
robbie-baxter <robbie.baxter@salesforce.com>
2019-12-19 13:23:18 -0500

    Finalize first simple test

--- 001_calc_pi/calcpi_test.go
+++ 001_calc_pi/calcpi_test.go
@@ -4,7 +4,12 @@ import "testing"
 
 func TestGetPi( t *testing.T) {
 	got := getPi(10)
-	if got != "3.14159265" {
+	if got != "3.141592653" {
 		t.Errorf("getPi(10) = %s; should be 3.141592653", got)
+	}
+
+	got = getPi(5)
+	if got != "3.1415" {
+		t.Errorf("getPi(5) = %s; should be 3.1415", got)
 	}
 }
