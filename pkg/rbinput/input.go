37002272a2ba16d8bebd1297f04495c70d14cea6
robbie-baxter <robbie.baxter@salesforce.com>
2019-12-24 13:42:23 -0500

    Finish up pkg changes to old files.

---
+++ pkg/rbinput/input.go
@@ -0,0 +1,19 @@
+package rbinput
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//function to take a message, and gather input from the user based on the message
+func GetInput(message string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(message)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSuffix(text, "\n")
+
+	return text
+}
