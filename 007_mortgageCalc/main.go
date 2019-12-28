1a789dc6275eacfc7eaf01af9d0492b204223d0d
robbie-baxter <robbie.baxter@salesforce.com>
2019-12-28 10:33:36 -0500

    Added percent to decimal conversion so that it doesn't suck at it's job

--- 007_mortgageCalc/main.go
+++ 007_mortgageCalc/main.go
@@ -10,9 +10,9 @@ import (
 func main() {
 	fmt.Println("Welcome to the Mortgage Calculator Application\n")
 
-	p, _ := strconv.ParseFloat(rbinput.GetInput("What is the principal amount?"), 64)
-	r, _ := strconv.ParseFloat(rbinput.GetInput("What is your annual interest rate?"), 64)
-	t, _ := strconv.ParseFloat(rbinput.GetInput("How many years is the term?"), 64)
+	p, _ := strconv.ParseFloat(rbinput.GetInput("What is the principal amount? "), 64)
+	r, _ := strconv.ParseFloat(rbinput.GetInput("What is your annual interest percentage rage? "), 64)
+	t, _ := strconv.ParseFloat(rbinput.GetInput("How many years is the term? "), 64)
 	//compounded daily by default
 	m := getMonthlyPayment(p,r,t)
 
@@ -22,8 +22,8 @@ func main() {
 // getMonthlyPayment calculates the monthly mortgage payment needed to pay off a loan in the given
 // amount of years. Formula taken from https://www.bankrate.com/calculators/mortgages/mortgage-calculator.aspx
 func getMonthlyPayment(principal float64, rate float64, term float64) float64{
-	//take the annual rate and change it to monthly
-	r := rate/12
+	//take the annual rate and change it to monthly and convert to decimal form percent
+	r := rate/12*.01
 	//number of monthly payments of the term (years * 12)
 	n := term * 12
 	// m = principal[r(1+r)^n/((1+r)^n)-1)]
